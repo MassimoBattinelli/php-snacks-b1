bcb2a10d02c993093128ae7e41c1a996e62b487e
massi <massimo.battinelli@gmail.com>
2022-04-08 18:33:44 +0200

    Snack 2

--- index.php
+++ index.php
@@ -14,27 +14,43 @@
 Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema. Olimpia Milano - Cantù | 55-60
 */
 
-$team1 = ['Napoli', 'Milan', 'Inter', 'Fiorentina', 'Manchester', 'Liverpool', 'Lazio', 'Roma'];
+// $team1 = ['Napoli', 'Milan', 'Inter', 'Fiorentina', 'Manchester', 'Liverpool', 'Lazio', 'Roma'];
 
-$basketMatches = [];
+// $basketMatches = [];
 
-for ($i = 0; $i < count($team1); $i++) {
+// for ($i = 0; $i < count($team1); $i++) {
 
-    if ($i < (count($team1) - 1)) {
-        $indexSecondTeam = $i+1;
-    } else {
-        $indexSecondTeam = 0;
-    }
+//     if ($i < (count($team1) - 1)) {
+//         $indexSecondTeam = $i+1;
+//     } else {
+//         $indexSecondTeam = 0;
+//     }
 
-    $randomTeam1Score = rand(1, 100);
-    $randomTeam2Score = rand(1, 100);
-    $basketMatches[] = [
-        "team1" => $team1[$i],
-        "team2" => $team1[$indexSecondTeam],
-        "team1Score" => $randomTeam1Score,
-        "team2Score" => $randomTeam2Score
-    ];
+//     $randomTeam1Score = rand(1, 100);
+//     $randomTeam2Score = rand(1, 100);
+//     $basketMatches[] = [
+//         "team1" => $team1[$i],
+//         "team2" => $team1[$indexSecondTeam],
+//         "team1Score" => $randomTeam1Score,
+//         "team2Score" => $randomTeam2Score
+//     ];
 
-    echo '<p>' . $basketMatches[$i]['team1'] . ' - ' . $basketMatches[$i]['team2'] . ' | ' . $basketMatches[$i]['team1Score'] . '-' . $basketMatches[$i]['team2Score'] . '</p>';
+//     echo '<p>' . $basketMatches[$i]['team1'] . ' - ' . $basketMatches[$i]['team2'] . ' | ' . $basketMatches[$i]['team1Score'] . '-' . $basketMatches[$i]['team2Score'] . '</p>';
+// }
+
+## Snack 2
+/*
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato"
+*/
+
+extract($_GET);
+
+$pattern = '/(?<=\@).*\./';
+
+if ((strlen($name) > 3) && preg_match($pattern, $mail) && is_numeric($age)) {
+    echo '<div>Accesso riuscito</div>';
+} else {
+    echo '<div>Accesso negato</div>';
 }
+
 
