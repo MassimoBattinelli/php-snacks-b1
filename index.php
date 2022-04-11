f35a8f013ba718696649cb0d52f55ee2f117e377
massi <massimo.battinelli@gmail.com>
2022-04-11 16:07:59 +0200

    fine progetto

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -43,14 +43,34 @@ Creiamo un array contenente le partite di basket di un'ipotetica tappa del calen
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato"
 */
 
-extract($_GET);
+// extract($_GET);
 
-$pattern = '/(?<=\@).*\./';
+// $pattern = '/(?<=\@).*\./';
 
-if ((strlen($name) > 3) && preg_match($pattern, $mail) && is_numeric($age)) {
-    echo '<div>Accesso riuscito</div>';
-} else {
-    echo '<div>Accesso negato</div>';
+// if ((strlen($name) > 3) && preg_match($pattern, $mail) && is_numeric($age)) {
+//     echo '<div>Accesso riuscito</div>';
+// } else {
+//     echo '<div>Accesso negato</div>';
+// } -->
+
+## Snack 4
+
+$randomNumbers = [];
+
+while (count($randomNumbers) < 15) {
+    $randomNumber = rand(1, 100);
+    if(!in_array($randomNumber, $randomNumbers)) {
+        $randomNumbers[] = $randomNumber;
+        echo $randomNumber . '<br>';
+    }
 }
 
+## Snack 5
 
+$portanza = "La portanza è definita come la forza aerodinamica agente in direzione perpendicolare alla direzione del vento che investe il profilo e permette al velivolo di alzarsi in volo. Essa è positiva se orientata verso l'alto. La portanza è generata dalla differenza di pressione fra la parte inferiore e quella superiore di un velivolo. Allo sviluppo della portanza può contribuire tutto il velivolo, ma il contributo maggiore è dovuto all'ala. La particolare forma del profilo alare permette di avere una differenza di velocità del fluido fra l'estradosso o dorso (parte superiore dell'ala) e l'intradosso o ventre (parte inferiore dell'ala). Conoscendo il coefficiente di portanza di un profilo (o di un corpo) è possibile risalire alla portanza";
+
+$paragraphsArr = explode('. ', $portanza);
+
+foreach ($paragraphsArr as &$value) {
+    echo '<p>' . $value . '</p>';
+}
